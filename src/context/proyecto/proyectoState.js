fcb6fa0f68437110954314f0c65a5b2e08d1c5ed
Mauri <mauricio.stampella@25watts.com.ar>
2021-02-06 12:56:48 -0300

    Llamando los proyectos con dispatch

--- src/context/proyecto/proyectoState.js
+++ src/context/proyecto/proyectoState.js
@@ -2,15 +2,19 @@ import React, {useReducer} from 'react';
 
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
-import { FORMULARIO_PROYECTO } from '../../types';
+import { FORMULARIO_PROYECTO, OBTENER_PROYECTOS } from '../../types';
+
 
 const ProyectoState = props => {
+    
+    const proyectos = [    
+        {id: 1, nombre: 'Tienda Virtual'},
+        {id: 2, nombre: 'Intranet'},
+        {id: 3, nombre: 'Diseño de Sitio Web'}
+    ]    
+
     const initialState = {
-        proyectos: [
-            {id: 1, nombre: 'Tienda Virtual'},
-            {id: 2, nombre: 'Intranet'},
-            {id: 3, nombre: 'Diseño de Sitio Web'}
-        ],
+        proyectos: [],
         formulario: false
     }
 
@@ -24,12 +28,21 @@ const ProyectoState = props => {
         })
     }
 
+    // Obtener los proyectos
+    const obtenerProyectos = () => {
+        dispatch({
+            type: OBTENER_PROYECTOS,
+            payload: proyectos
+        })
+    }
+
     return(
         <proyectoContext.Provider
             value={{
                 proyectos: state.proyectos,
                 formulario: state.formulario,
-                mostrarFormulario
+                mostrarFormulario,
+                obtenerProyectos
             }}
         >
             {props.children}
