6a9275e4ab990aad8f2e46ec28681084c447c265
Mauri <mauricio.stampella@25watts.com.ar>
2021-02-06 12:41:58 -0300

    Agregando los Proyectos con Context

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Proyecto from './Proyecto';
+import proyectoContext from '../../context/proyecto/proyectoContext';
 
 const ListadoProyectos = () => {
 
-    const proyectos = [
-        {nombre: 'Tienda Virtual'},
-        {nombre: 'Intranet'},
-        {nombre: 'Diseño de Sitio Web'}
-    ]
+    // Extraer proyectos de state inicial
+    const proyectosContext = useContext(proyectoContext);
+    const {proyectos} = proyectosContext;
+
+    // Revisar si proyectos tiene contenido
+    if( proyectos.length === 0 ) return null;
 
     return ( 
         <ul className="listado-proyectos">
             {proyectos.map(proyecto => (
                 <Proyecto
+                    key={proyecto.id}
                     proyecto={proyecto}
                 />
             ))}
