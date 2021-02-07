b845d39f3670f56ea6d8059eb9ec494513cdfb9e
Mauri <mauricio.stampella@25watts.com.ar>
2021-02-07 10:38:32 -0300

    Mostrando el nombre del proyecto

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,7 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import Tarea from './Tarea';
+import proyectoContext from '../../context/proyecto/proyectoContext';
 
 const ListadoTareas = () => {
+
+    // Obtener el state del Proyectos
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    // Si no hay proyecto seleccionado
+    if(!proyecto){
+        return <h2>Seleccionar un proyecto</h2>;
+    }
+
+    // Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
 
     const tareasProyecto = [
         { nombre: 'Elegir Plataforma', estado: true },
@@ -10,9 +23,11 @@ const ListadoTareas = () => {
         { nombre: 'Elegir Hosting', estado: true },
     ]
 
+    
+
     return ( 
         <Fragment>
-            <h2>Proyecto: TiendaVirtual</h2>
+            <h2>Proyecto: {proyectoActual.nombre}</h2>
 
             <ul className="listado-tareas">
                 { tareasProyecto.length === 0
