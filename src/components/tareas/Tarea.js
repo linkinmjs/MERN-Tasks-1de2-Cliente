970a0cb16380d5e0783c7809a65dc0ed04deba98
Mauri <mauricio.stampella@25watts.com.ar>
2021-02-22 09:43:45 -0300

    Modificar el Estado de una Tarea

--- src/components/tareas/Tarea.js
+++ src/components/tareas/Tarea.js
@@ -11,7 +11,7 @@ const Tarea = ({tarea}) => {
 
     // Obtener funciones del context de tarea
     const tareasContext = useContext(tareaContext);
-    const { eliminarTarea, obtenerTareas } = tareasContext;
+    const { eliminarTarea, obtenerTareas, cambiarEstadoTarea } = tareasContext;
     
     // Extraer el proyecto
     const [ proyectoActual ] = proyecto;
@@ -20,6 +20,16 @@ const Tarea = ({tarea}) => {
     const tareaEliminar = id => {
         eliminarTarea(id);
         obtenerTareas(proyectoActual.id)
+    }
+
+    // Función que modifica el estado de las tareas
+    const cambiarEstado = tarea => {
+        if(tarea.estado) {
+            tarea.estado = false;
+        }else{
+            tarea.estado = true
+        }
+        cambiarEstadoTarea(tarea);
     }
 
     return (
@@ -32,6 +42,7 @@ const Tarea = ({tarea}) => {
                             <button
                                 type="button"
                                 className="completo"
+                                onClick={() => cambiarEstado(tarea)}
                             >Completo</button>
                         )
                     :
@@ -39,6 +50,7 @@ const Tarea = ({tarea}) => {
                             <button
                                 type="button"
                                 className="incompleto"
+                                onClick={() => cambiarEstado(tarea)}
                             >Incompleto</button>
                         )                    
                     }
