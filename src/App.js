0ff82522b2ff50e0ad9f6c8c30e65f30513c618b
Mauri <mauricio.stampella@25watts.com.ar>
2021-02-26 19:42:01 -0300

    Creando el State y Provider

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Proyectos from './components/proyectos/Proyectos';
 import ProyectoState from './context/proyectos/proyectoState';
 import TareaState from './context/tareas/tareaState';
 import AlertaState from './context/alertas/alertaState';
+import AuthState from './context/autenticacion/authState';
 
 
 function App() {
@@ -13,13 +14,15 @@ function App() {
     <ProyectoState>
       <TareaState>
         <AlertaState>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-              <Route exact path="/proyectos" component={Proyectos} />
-            </Switch>
-          </Router>
+          <AuthState>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+                <Route exact path="/proyectos" component={Proyectos} />
+              </Switch>
+            </Router>
+          </AuthState>
         </AlertaState>
       </TareaState>
     </ProyectoState>
