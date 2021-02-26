59f892781741c1a0ec52934bb04346164e2d6162
Mauri <mauricio.stampella@25watts.com.ar>
2021-02-26 20:04:18 -0300

    Leyendo datos del formulario de React y enviandolos a la API

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,12 +1,16 @@
 import React, {useState, useContext} from 'react';
 import { Link } from 'react-router-dom'
 import AlertaContext from '../../context/alertas/alertaContext';
+import AuthContext from '../../context/autenticacion/authContext'
 
 const NuevaCuenta = () => {
 
     // Extraer los valores del context
     const alertaContext = useContext(AlertaContext);
     const { alerta, mostrarAlerta } = alertaContext;
+
+    const authContext = useContext(AuthContext);
+    const { registrarUsuario } = authContext;
 
     // State para iniciar Sesión
     const [usuario, guardarUsuario] = useState({
@@ -52,7 +56,11 @@ const NuevaCuenta = () => {
         }
 
         // Pasarlo al Action
-        
+        registrarUsuario({
+            nombre,
+            email,
+            password
+        });
     }
 
     return ( 
