c43b42f5409682645bc4eff110c8c3d0e42d8b13
Mauri <mauricio.stampella@25watts.com.ar>
2021-02-27 11:43:49 -0300

    Obteniendo información del usuario autenticado

--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -32,7 +32,10 @@ const AuthState = props => {
             dispatch({
                 type: REGISTRO_EXITOSO,
                 payload: respuesta.data
-            })
+            });
+
+            // Obtener el usuario
+            usuarioAutenticado();
         } catch (error) {
             //console.log(error.response.data.msg);
             const alerta = {
@@ -46,6 +49,24 @@ const AuthState = props => {
             })
         }
     }
+
+    // Retorna el usuario autenticado
+    const usuarioAutenticado = async () => {
+        const token = localStorage.getItem('token');
+        if(token){
+            // TODO: Función para enviar el token por headers
+        }
+
+        try {
+            const respuesta = await clienteAxios.get('/api/auth');
+            console.log(respuesta);
+        } catch (error) {
+            dispatch({
+                type: LOGIN_ERROR
+            })
+        }
+    }
+    
 
     return(
         <AuthContext.Provider
