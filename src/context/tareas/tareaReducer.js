c9f91516c940d5ca6e40ee14d02fd2d16c2e7e55
Mauri <mauricio.stampella@25watts.com.ar>
2021-03-01 11:18:14 -0300

    Insertando Tareas

--- src/context/tareas/tareaReducer.js
+++ src/context/tareas/tareaReducer.js
@@ -16,7 +16,7 @@ export default (state, action) => {
         case TAREAS_PROYECTO:
             return{
                 ...state,
-                tareasproyecto: state.tareas.filter(
+                tareasproyecto: state.tareasproyecto.filter(
                         tarea => tarea.proyectoId === action.payload
                     )
             }
@@ -24,7 +24,7 @@ export default (state, action) => {
         case AGREGAR_TAREA:
             return{
                 ...state,
-                tareas: [action.payload, ...state.tareas],
+                tareasproyecto: [action.payload, ...state.tareasproyecto],
                 errortarea:false
             }
 
@@ -37,14 +37,14 @@ export default (state, action) => {
         case ELIMINAR_TAREA: 
             return{
                 ...state,
-                tareas: state.tareas.filter( tarea => tarea.id !== action.payload )
+                tareasproyecto: state.tareasproyecto.filter( tarea => tarea.id !== action.payload )
             }
 
         case ACTUALIZAR_TAREA:
         case ESTADO_TAREA:
             return{
                 ...state,
-                tareas: state.tareasproyecto.map(tarea => tarea.id === action.payload.id ? action.payload : tarea)
+                tareasproyecto: state.tareasproyecto.map(tarea => tarea.id === action.payload.id ? action.payload : tarea)
             }
         
         case TAREA_ACTUAL:
